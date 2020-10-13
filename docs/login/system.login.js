2ea194c5ffa0ca79223c74fb1e6bed032da54ee1
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2020-10-14 04:58:57 +0900

    login and chrypt,and special class

--- docs/login/system.login.js
+++ docs/login/system.login.js
@@ -1,23 +1,35 @@
-﻿function lg_form() {
-  session_do();
+﻿if (session_do("check", "")) {
+  location.href = "/";
+} else {
+  location.href = "/login/";
+}
+function lg_form() {
+  let data;
+  data.grp = document.login_form.grp.value;
+  data.id = document.login_form.id.value;
+  let shaObj = new jsSHA("SHA3-512", "TEXT", { encoding: "UTF8" });
+  sha3obj.update(document.login_form.pass.value);
+  data.pass = shaObj.getHash("HEX");
+  data.code = document.login_form.code.value;
+  session_do("save", data);
 }
 function session_do(type, data) {
   switch (type) {
     case "save":
-      sessionStorage.setItem(data.key, data.username);
+      sessionStorage.setItem("id", data.id);
       return true;
     case "get":
       if (session_do("check", data)) {
-        data.username = sessionStorage.getItem(data.key);
+        data.id = sessionStorage.getItem("id");
         return data;
       } else {
         return false;
       }
     case "delete":
-      sessionStorage.removeItem(data.key);
+      sessionStorage.removeItem("id");
       return true;
     case "check":
-      if (!sessionStorage.getItem(data.key)) {
+      if (!sessionStorage.getItem("id")) {
         return false;
       } else {
         return true;
